2c73b5cafb61bd0548a62d793a5d0c917d2b3c26
Nassim <bouyacoub@essec.fr>
2024-10-23 11:20:13 +0200

    all

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Iframe from './components/Iframe';
+import All from './components/All';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/:lang/:id/' element={<Iframe />}/> 
-        <Route path='/' element={<h1>hi</h1>}/> 
+        <Route path='/:lang' element={<All/>}/> 
 
       </Routes>
     </Router>
---
+++ src/components/All.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+function All() {
+
+    const slug=useParams()
+    var langue=""
+    if(slug.lang==="fr"){
+        langue="fr-FR"
+      }
+      else if(slug.lang==="en")
+        {langue="en-US"}
+  return (
+        <div >
+            <iframe style={{ height: '98vh' }} data-ub-cookie-consent="necessary" data-ub-lang={langue} id="unibuddy-iframe" scrolling="no" src="https://unibuddy.co/embed/essec-business-school/colour/1da1e0" title="Unibuddy" width="100%">
+            </iframe>
+            <script src="https://cdn.unibuddy.co/unibuddy-iframe.js" type="text/javascript">
+            </script>
+
+        </div>
+  );
+}
+
+export default All;
