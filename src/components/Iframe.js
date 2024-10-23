befe4599d662f64b85f6b492adba0b0e1af20a4c
Nassim <bouyacoub@essec.fr>
2024-10-23 10:53:31 +0200

    finish

---
+++ src/components/Iframe.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+function Iframe() {
+  const slug=useParams()
+  console.log(slug)
+  var langue=""
+  if(slug.lang==="fr"){
+    langue="fr-FR"
+  }
+  else if(slug.lang==="en")
+    {langue="en-US"}
+  console.log(langue)
+  var slugUni="https://unibuddy.co/embed/essec-business-school/course/"+slug.id
+  
+  
+  
+  return (
+    <div >
+  <iframe style={{ height: '98vh' }} data-ub-cookie-consent="necessary" data-ub-lang={langue} id="unibuddy-iframe" scrolling="no" src={slugUni} title="Unibuddy" width="100%">
+  </iframe>
+  <script src="https://cdn.unibuddy.co/unibuddy-iframe.js" type="text/javascript">
+  </script>
+    </div>
+  );
+}
+
+export default Iframe;
