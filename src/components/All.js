3582d44e2acf9c9dfeeeb8d852935cd5f803f6ae
Nassim <bouyacoub@essec.fr>
2024-10-24 10:00:28 +0200

    ajout du scrolling

--- src/components/All.js
+++ src/components/All.js
@@ -12,7 +12,7 @@ function All() {
         {langue="en-US"}
   return (
         <div >
-            <iframe style={{ height: '98vh',border:"0px" }} data-ub-cookie-consent="necessary" data-ub-lang={langue} id="unibuddy-iframe" scrolling="no" src="https://unibuddy.co/embed/essec-business-school/colour/1da1e0" title="Unibuddy" width="100%">
+            <iframe style={{ height: '98vh',border:"0px" }} data-ub-cookie-consent="necessary" data-ub-lang={langue} id="unibuddy-iframe" scrolling="yes" src="https://unibuddy.co/embed/essec-business-school/colour/1da1e0" title="Unibuddy" width="100%">
             </iframe>
             <script src="https://cdn.unibuddy.co/unibuddy-iframe.js" type="text/javascript">
             </script>
