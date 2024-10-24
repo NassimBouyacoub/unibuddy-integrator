1a07eda325ae29b9044eadcb0573d53e3bfc4ffe
Nassim <bouyacoub@essec.fr>
2024-10-24 10:45:08 +0200

    test with script in head

--- src/components/All.js
+++ src/components/All.js
@@ -14,9 +14,6 @@ function All() {
         <div >
             <iframe style={{ height: '98vh',border:"0px" }} data-ub-cookie-consent="necessary" data-ub-lang={langue} id="unibuddy-iframe" scrolling="yes" src="https://unibuddy.co/embed/essec-business-school/colour/1da1e0" title="Unibuddy" width="100%">
             </iframe>
-            <script src="https://cdn.unibuddy.co/unibuddy-iframe.js" type="text/javascript">
-            </script>
-
         </div>
   );
 }
