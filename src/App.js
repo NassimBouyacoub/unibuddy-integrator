98258a95dcda65110e2f06878d0afab53faecfd7
Nassim <bouyacoub@essec.fr>
2024-11-08 11:20:22 +0100

    coreection affichage

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
 import { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Iframe from './components/Iframe';
 import All from './components/All';
-import { useLocation } from 'react-router-dom';
 import ReactGA from 'react-ga4';
 
 const TRACKING_ID = "G-TR9J7V862N"; // Remplacez par votre propre ID GA4
 ReactGA.initialize(TRACKING_ID);
 
-function App() {
+function AnalyticsTracker() {
   const location = useLocation();
   useEffect(() => {
     ReactGA.send("pageview", location.pathname + location.search);
+  }, [location]);
+  
+  return null;
+}
 
-    ReactGA.send("pageview");
+function App() {
+  useEffect(() => {
     const script = document.createElement('script');
     script.src = 'https://cdn.unibuddy.co/unibuddy-iframe.js';
-    // script.async = true; // optionnel : pour un chargement asynchrone
     document.body.appendChild(script);
     return () => {
       document.body.removeChild(script); // Nettoyage si nécessaire
     };
-  }, [location]);
-    return (
-    <Router>
-      <Routes>
-        <Route path='/:lang/:id/' element={<Iframe />}/> 
-        <Route path='/:lang' element={<All/>}/> 
+  }, []);
 
-      </Routes>
-    </Router>
+  return (
+    <Routes>
+      <Route path='/:lang/:id/' element={<Iframe />}/> 
+      <Route path='/:lang' element={<All/>}/> 
+    </Routes>
   );
 }
 
-export default App;
+export default function MainApp() {
+  return (
+    <Router>
+      <AnalyticsTracker />
+      <App />
+    </Router>
+  );
+}
