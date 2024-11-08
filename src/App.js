838cda6aa8e9cc246185900d0032a5d4c36e4a6c
Nassim <bouyacoub@essec.fr>
2024-11-08 11:05:00 +0100

    Google Analytics

--- src/App.js
+++ src/App.js
@@ -3,12 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Iframe from './components/Iframe';
 import All from './components/All';
 
-import ReactGA from 'react-ga';
-const TRACKING_ID = "G-TR9J7V862N";
-
 function App() {
   useEffect(() => {
-    ReactGA.initialize(TRACKING_ID);
     const script = document.createElement('script');
     script.src = 'https://cdn.unibuddy.co/unibuddy-iframe.js';
     // script.async = true; // optionnel : pour un chargement asynchrone
